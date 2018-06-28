5b1cfca8ef660513b663f29a1e93d100b877fdcd
Alonso <alons45@gmail.com>
2018-06-28 16:02:44 -0500

    add initial pseudocode

--- sea.py
+++ sea.py
@@ -1,8 +1,58 @@
+import arcade
+
 from diver import Diver
 from turtle import Turtle
 
+""" Sea
 
-class Sea:
-    def __init__(self):
-        self.turtle = Turtle()
-        self.driver = Diver()
+turtle_population: POSITIVE INTEGER
+turtle: Turtle OBJECT
+divers: Diver OBJECT LIST
+is_enough_dives_hunting: BOOLEAN
+natural_sounds: STRING
+
+Algorithm: update
+Description:
+This algorithm is a loop when the main actions happen
+
+START    
+    game_over()
+    won()
+
+    enough_hunting_dives()    
+
+    turtle.movement()    
+END
+
+
+Algorithm: game_over
+
+START
+    IF turtle is touch for divers
+        RETURN True
+    ELSE
+        RETURN False
+END
+
+Algorithm: won
+
+START
+    IF turtle reach final
+        RETURN True
+    ELSE
+        RETURN False
+END
+
+Algorithm: enough_dives_hunting
+
+START
+    IF exists enough dives hunting (enough_dives_hunting) the turtle
+        GENERATE dives
+
+END
+
+"""
+
+
+class Sea(arcade.Window):
+    pass
